b410b6dc6a2cd57186213cc3dd561a69b34f0b43
cuilin <rinchoi000@yeah.net>
2020-09-02 11:52:16 +0800

    commit offer-20

---
+++ view/offer-20.py
@@ -0,0 +1,113 @@
+# 表示数值的字符串
+# medium
+'''
+请实现一个函数用来判断字符串是否表示数值（包括整数和小数）。
+例如，字符串"+100"、"5e2"、"-123"、"3.1416"、"-1E-16"、"0123"都表示数值，
+但"12e"、"1a3.14"、"1.2.3"、"+-5"及"12e+5.4"都不是。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/biao-shi-shu-zhi-de-zi-fu-chuan-lcof
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+--------------------------------------------
+题解：有限状态机
+定义一个状态列表，一个行为表，一个状态里可以做不同的行为，一个行为后跟着下一个状态。
+通过这样两个表可以生成一个状态转移表，在此题里可以把是否已有符号，是否已有小数点等看成状态，
+而行为就是允许出现的字符，这样就能形成一个优先状态机，如果能转移到end状态，那表明此字符串是数字
+
+'''
+
+
+from enum import Enum
+
+class Solution:
+    def isNumber(self, s: str) -> bool:
+        State = Enum("State", [
+            "STATE_INITIAL",
+            "STATE_INT_SIGN",
+            "STATE_INTEGER",
+            "STATE_POINT",
+            "STATE_POINT_WITHOUT_INT",
+            "STATE_FRACTION",
+            "STATE_EXP",
+            "STATE_EXP_SIGN",
+            "STATE_EXP_NUMBER",
+            "STATE_END",
+        ])
+        Chartype = Enum("Chartype", [
+            "CHAR_NUMBER",
+            "CHAR_EXP",
+            "CHAR_POINT",
+            "CHAR_SIGN",
+            "CHAR_SPACE",
+            "CHAR_ILLEGAL",
+        ])
+
+        def toChartype(ch: str) -> Chartype:
+            if ch.isdigit():
+                return Chartype.CHAR_NUMBER
+            elif ch.lower() == "e":
+                return Chartype.CHAR_EXP
+            elif ch == ".":
+                return Chartype.CHAR_POINT
+            elif ch == "+" or ch == "-":
+                return Chartype.CHAR_SIGN
+            elif ch == " ":
+                return Chartype.CHAR_SPACE
+            else:
+                return Chartype.CHAR_ILLEGAL
+        
+        transfer = {
+            State.STATE_INITIAL: {
+                Chartype.CHAR_SPACE: State.STATE_INITIAL,
+                Chartype.CHAR_NUMBER: State.STATE_INTEGER,
+                Chartype.CHAR_POINT: State.STATE_POINT_WITHOUT_INT,
+                Chartype.CHAR_SIGN: State.STATE_INT_SIGN,
+            },
+            State.STATE_INT_SIGN: {
+                Chartype.CHAR_NUMBER: State.STATE_INTEGER,
+                Chartype.CHAR_POINT: State.STATE_POINT_WITHOUT_INT,
+            },
+            State.STATE_INTEGER: {
+                Chartype.CHAR_NUMBER: State.STATE_INTEGER,
+                Chartype.CHAR_EXP: State.STATE_EXP,
+                Chartype.CHAR_POINT: State.STATE_POINT,
+                Chartype.CHAR_SPACE: State.STATE_END,
+            },
+            State.STATE_POINT: {
+                Chartype.CHAR_NUMBER: State.STATE_FRACTION,
+                Chartype.CHAR_EXP: State.STATE_EXP,
+                Chartype.CHAR_SPACE: State.STATE_END,
+            },
+            State.STATE_POINT_WITHOUT_INT: {
+                Chartype.CHAR_NUMBER: State.STATE_FRACTION,
+            },
+            State.STATE_FRACTION: {
+                Chartype.CHAR_NUMBER: State.STATE_FRACTION,
+                Chartype.CHAR_EXP: State.STATE_EXP,
+                Chartype.CHAR_SPACE: State.STATE_END,
+            },
+            State.STATE_EXP: {
+                Chartype.CHAR_NUMBER: State.STATE_EXP_NUMBER,
+                Chartype.CHAR_SIGN: State.STATE_EXP_SIGN,
+            },
+            State.STATE_EXP_SIGN: {
+                Chartype.CHAR_NUMBER: State.STATE_EXP_NUMBER,
+            },
+            State.STATE_EXP_NUMBER: {
+                Chartype.CHAR_NUMBER: State.STATE_EXP_NUMBER,
+                Chartype.CHAR_SPACE: State.STATE_END,
+            },
+            State.STATE_END: {
+                Chartype.CHAR_SPACE: State.STATE_END,
+            },
+        }
+
+        st = State.STATE_INITIAL
+        for ch in s:
+            typ = toChartype(ch)
+            if typ not in transfer[st]:
+                return False
+            st = transfer[st][typ]
+        
+        return st in [State.STATE_INTEGER, State.STATE_POINT, State.STATE_FRACTION, State.STATE_EXP_NUMBER, State.STATE_END]
