87eaa4876b2508f81dd19c6d3823a71a7a0d7594
cuilin <rinchoi000@yeah.net>
2020-09-08 14:33:04 +0800

    commit al-46 al-77

---
+++ al/al-46.py
@@ -0,0 +1,48 @@
+# 全排列
+# medium
+'''
+给定一个 没有重复 数字的序列，返回其所有可能的全排列。
+
+示例:
+
+输入: [1,2,3]
+输出:
+[
+  [1,2,3],
+  [1,3,2],
+  [2,1,3],
+  [2,3,1],
+  [3,1,2],
+  [3,2,1]
+]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/permutations
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+-------------------------------------------
+题解：回溯
+全排列就是元素之间的交换，一个循环，将第一个元素和后面的每一个交换，交换后，剩下的[1:]数组继续递归，
+当然也可以不交换进行下一步递归，不交换就是回溯
+
+'''
+
+import copy
+
+class Solution:
+    def permute(self, nums):
+        ans = []
+        def backstrap(cur):
+            if cur == len(nums) - 1:
+                tmp = copy.deepcopy(nums)
+                ans.append(tmp)
+                return
+            for i in range(cur, len(nums)):
+                nums[cur], nums[i] = nums[i], nums[cur]
+                backstrap(cur + 1)
+                nums[cur], nums[i] = nums[i], nums[cur]
+        backstrap(0)
+        return ans
+
+
+print(Solution().permute([1,2,3]))
---
+++ al/al-77.py
@@ -0,0 +1,51 @@
+# 组合
+# medium
+'''
+给定两个整数 n 和 k，返回 1 ... n 中所有可能的 k 个数的组合。
+
+示例:
+
+输入: n = 4, k = 2
+输出:
+[
+  [2,4],
+  [3,4],
+  [2,3],
+  [1,2],
+  [1,3],
+  [1,4],
+]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/combinations
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+------------------------------------------
+题解：回溯
+典型回溯题, +1-1 的做就行
+
+'''
+
+import copy
+
+class Solution:
+    def combine(self, n: int, k: int):
+        ans, group = [], []
+        def backstrap(num):
+            if len(group) == k:
+                tmp = copy.deepcopy(group)
+                ans.append(tmp)
+                return
+            if num > n:
+                return
+            group.append(num)
+            backstrap(num + 1)
+            group.pop()
+            backstrap(num + 1)
+        
+        backstrap(1)
+        return ans
+            
+
+print(Solution().combine(4, 2))
+        
